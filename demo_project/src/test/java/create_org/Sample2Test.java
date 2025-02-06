0a81363d2861272ce5b0cc64679ed2f0109ca210
prem <premkumar9585@gmail.com>
2025-02-06 19:58:08 +0530

    push1

---
+++ demo_project/src/test/java/create_org/Sample2Test.java
@@ -0,0 +1,16 @@
+package create_org;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+@Test(groups= "smoke test")
+	public void contactmodified()
+	{
+		System.out.println("contact modified  for qspider login");
+	}
+	@Test(groups= "smoke test")
+	public void contactdeleted()
+	{
+		System.out.println("t contactdeleted  for qspider login");
+	}
+}
---
+++ demo_project/src/test/java/create_org/SampleTest.java
@@ -0,0 +1,11 @@
+package create_org;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test (groups ="smoke test")
+	public void CreateContact()
+	{
+		System.out.println("contact created for qspider login ");
+	}
+}
